72bc5dcab567e41b5781c897c9e056371e996f33
MedKms <mohamed.kemmous-etu@etu.univh2c.ma>
2022-03-14 11:46:29 +0100

    add Logger & logback-spring.xml

--- src/main/java/com/example/springbatchunivocity/services/readFilesService/readFilesServiceImpl/ReaderCsv.java
+++ src/main/java/com/example/springbatchunivocity/services/readFilesService/readFilesServiceImpl/ReaderCsv.java
@@ -2,8 +2,11 @@ package com.example.springbatchunivocity.services.readFilesService.readFilesServ
 
 import com.example.springbatchunivocity.model.Student;
 import com.example.springbatchunivocity.services.readFilesService.Reader;
+import com.univocity.parsers.common.DataProcessingException;
 import com.univocity.parsers.csv.CsvParserSettings;
 import com.univocity.parsers.csv.CsvRoutines;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.stereotype.Component;
 
@@ -11,21 +14,27 @@ import java.io.File;
 import java.util.Iterator;
 @Component
 public class ReaderCsv implements Reader {
+    private final Logger logger= LoggerFactory.getLogger(this.getClass());
     private Iterator<Student> iterator;
     @Override
     public ItemReader<Student> readFile(String path) {
         return () -> {
-            File file=new File(path);
-            if(iterator == null){
-                CsvParserSettings parserSettings = new CsvParserSettings();
-                CsvRoutines routines = new CsvRoutines(parserSettings);
-                iterator = routines.iterate(Student.class, file).iterator();
+            try{
+                File file=new File(path);
+                if(iterator == null){
+                    CsvParserSettings parserSettings = new CsvParserSettings();
+                    CsvRoutines routines = new CsvRoutines(parserSettings);
+                    iterator = routines.iterate(Student.class, file).iterator();
+                }
+                if(iterator.hasNext()){
+                    return iterator.next();
+                } else {
+                    iterator = null;
+                }
+            }catch (RuntimeException exception){
+                logger.error(exception.getMessage());
             }
-            if(iterator.hasNext()){
-                return iterator.next();
-            } else {
-                iterator = null;
-            }
+
             return null;
         };
     }
